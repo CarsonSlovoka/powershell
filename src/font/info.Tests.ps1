7e29814ddc56f1729e4254e607157074d12a3545
Carson <jackparadise520a@gmail.com>
2023-06-09 17:09:30 +0800

    修正可能因為機碼或者Arial字型不在而發生測試錯誤的問題

--- src/font/info.Tests.ps1
+++ src/font/info.Tests.ps1
@@ -1,21 +1,29 @@
 Describe "[font.info.psm1]" {
+    if (!(
+        (Test-Path -Path "HKCU:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts") -and
+        (Test-Path -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts")
+      )) {
+        Write-Host '"SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts" not found'
+        return # 就不繼續往下測試;
+    }
+
+    $fontPath = Join-Path $env:SystemRoot fonts/Arial.ttf
+    if (!(Test-Path $fontPath)) {
+        Write-Host "$fontPath not found"
+        return
+    }
+
     BeforeAll {
         Import-Module (Join-Path $PSScriptRoot 'info.psm1')
         $fontPath = Join-Path $env:SystemRoot fonts/Arial.ttf
     }
+
     It "Calls Get-GlyphTypeface" {
-        if (Test-Path $fontPath) {
-            $glyphTypeface = Get-GlyphTypeface $fontPath -ErrorAction SilentlyContinue
-            $glyphTypeface | Should -Not -BeNullOrEmpty
-        }
+        $glyphTypeface = Get-GlyphTypeface $fontPath -ErrorAction SilentlyContinue
+        $glyphTypeface | Should -Not -BeNullOrEmpty
     }
 
     It "Calls Get-InstallGlyphTypeface" {
-        if (!(Test-Path $fontPath)) {
-            Write-Host "$fontPath not found"
-            return
-        }
-
         $glyphTypefaceMap = Get-InstallGlyphTypeface -location CurrentUser -ErrorAction SilentlyContinue
         $glyphTypefaceMap | Should -Not -BeNullOrEmpty
         $glyphTypefaceMap = Get-InstallGlyphTypeface -location LocalMachine -ErrorAction SilentlyContinue

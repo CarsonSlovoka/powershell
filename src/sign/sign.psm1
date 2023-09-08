6fcf65f30d1dd90c91583feda35333593674f127
Carson <jackparadise520a@gmail.com>
2023-09-08 12:19:51 +0800

    先嘗試用gcm尋找再嘗試智能搜尋

--- src/sign/sign.psm1
+++ src/sign/sign.psm1
@@ -14,6 +14,12 @@ function Add-DigitalSignup {
             (Get-Item 'C:\xxx\my.pfx') `
             -password '123' `
             -subjectName 'xxx Taiwan Inc'
+    .Example
+        Add-DigitalSignup (Get-Item 'C:\..\my.exe') `
+            (Get-Item 'C:\xxx\my.crt') `
+            (Get-Item 'C:\xxx\my.pfx') `
+            -password '123' `
+            -subjectName 'xxx Taiwan Inc' -Verbose
     .Link
         https://learn.microsoft.com/en-us/dotnet/framework/tools/signtool-exe
     #>
@@ -37,20 +43,31 @@ function Add-DigitalSignup {
         [string]$tURL = 'http://timestamp.digicert.com' # /t
     )
 
-    # $orgLocation = (pwd).Path
-    # cd (Join-Path ${env:ProgramFiles(x86)} 'Windows Kits/')
+    $signToolExePath = (gcm signtool.exe -ErrorAction SilentlyContinue).Path
 
-    <#
-    ${env:ProgramFiles(x86)}\Windows Kits\10\App Certification Kit\signtool.exe <-- we want this one.
-    ${env:ProgramFiles(x86)}\Windows Kits\10\bin\10.0.17763.0\arm\signtool.exe
-    ...
-    ${env:ProgramFiles(x86)}\Windows Kits\10\bin\10.0.17763.0\x64\signtool.exe
-    #>
-    # $signToolExePath = Join-Path ${env:ProgramFiles(x86)} 'Windows Kits/10/App Certification Kit/signtool.exe'
-    $signToolExePath = Get-ChildItem -Path (Join-Path ${env:ProgramFiles(x86)} 'Windows Kits/') -Recurse -Filter "*signtool.exe" -File `
-        | Select-Object -ExpandProperty FullName ` # return Object[]
-        | Where-Object {$_ -like '*App Certification Kit*'} `
-        | Select-Object -First 1
+    if ($signToolExePath -eq $null) {
+        Write-Verbose ('"{0}" was not found in the system path. An intelligent search will be attempted.' -f 'signTool.exe')
+
+        # $orgLocation = (pwd).Path
+        # cd (Join-Path ${env:ProgramFiles(x86)} 'Windows Kits/')
+
+        <#
+        ${env:ProgramFiles(x86)}\Windows Kits\10\App Certification Kit\signtool.exe <-- we want this one.
+        ${env:ProgramFiles(x86)}\Windows Kits\10\bin\10.0.17763.0\arm\signtool.exe
+        ...
+        ${env:ProgramFiles(x86)}\Windows Kits\10\bin\10.0.17763.0\x64\signtool.exe
+        #>
+        # $signToolExePath = Join-Path ${env:ProgramFiles(x86)} 'Windows Kits/10/App Certification Kit/signtool.exe'
+        $signToolExePath = Get-ChildItem -Path (Join-Path ${env:ProgramFiles(x86)} 'Windows Kits/') -Recurse -Filter "*signtool.exe" -File `
+            | Select-Object -ExpandProperty FullName `
+            | Where-Object {$_ -like '*App Certification Kit*'} `
+            | Select-Object -First 1
+    }
+
+    if ($signToolExePath -eq $null) {
+        Write-Error "couldn't found the signTool.exe"
+        return
+    }
 
     $params = @(
         # required
@@ -71,6 +88,6 @@ function Add-DigitalSignup {
 
     $params += '"{0}"' -f $exePath
 
-    # echo $signToolExePath ([String]::Join(' ', $params))
+    Write-Verbose "$signToolExePath $([String]::Join(' ', $params))"
     Start-Process $signToolExePath ([String]::Join(' ', $params))
 }

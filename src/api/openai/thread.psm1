1bb21b5088c2398febc5044f9d87ced7bcf0f6a7
Carson <jackparadise520a@gmail.com>
2024-03-12 16:32:29 +0800

    優化OpenAI-ListThread

--- src/api/openai/thread.psm1
+++ src/api/openai/thread.psm1
@@ -13,9 +13,17 @@ function Request-OpenAI-ListThread {
         n如果是1的時候會跑很快, 越大的n會跑越久
         1~100之間
     .EXAMPLE
-        Get-OpenAI-AllThread sess-1234567890123456789012345678901234567890
+        Request-OpenAI-ListThread  sess-1234567890123456789012345678901234567890
     .EXAMPLE
-        Get-OpenAI-AllThread sess-1234567890123456789012345678901234567890 5
+        # 僅列出所有ThreadID
+        Request-OpenAI-ListThread  sess-... | foreach { $_.id }
+        $tIDs = Request-OpenAI-ListThread  sess-... | foreach { $_.id }
+    .EXAMPLE
+        # 注意這個tIDs傳入給Request-OpenAI-GetThreads還是會遇到問題
+        $tIDs = @()
+        Request-OpenAI-ListThread  sess-... | foreach { $tIDs += $_.id }
+    .EXAMPLE
+        Request-OpenAI-ListThread  sess-... 5
     .Link
         https://platform.openai.com/assistants
         https://community.openai.com/t/list-of-threads-is-missing-from-the-api/484510/28
@@ -27,9 +35,22 @@ function Request-OpenAI-ListThread {
         [int]$limit=5
     )
 
-    curl "https://api.openai.com/v1/threads?&limit=$limit" `
+    $out = curl "https://api.openai.com/v1/threads?&limit=$limit" `
       -H "Authorization: Bearer $key" `
       -H "OpenAI-Beta: assistants=v1"
+
+    $out
+
+    $out = $out | ConvertFrom-Json # 將字串轉成json物件
+
+    $info = @()
+    foreach ($th in $out.data) {
+        $info += @{
+            id = $th.id
+            created_at = Get-Date -UnixTimeSeconds $th.created_at # 轉成看得懂的日期
+        }
+    }
+    $info
 }
 
 function Request-OpenAI-GetThreads {
@@ -49,6 +70,11 @@ function Request-OpenAI-GetThreads {
     .EXAMPLE
         # 查詢多筆
         Request-OpenAI-GetThreads @("thread_123456789012345678901234", thread_...")
+    .EXAMPLE
+        $threads = @(
+            'thread_...'
+        )
+        Request-OpenAI-GetThreads $threads
     .LINK
         https://platform.openai.com/playground?assistant=<assistantID>&mode=assistant&thread=<threadID>
     #>
@@ -63,11 +89,26 @@ function Request-OpenAI-GetThreads {
     # $out = @()
     foreach ($threadID in $ids) {
         Write-Host "查詢此Thread: $threadID" -ForegroundColor Green # 開始前先打印出來，方便曉得已經換成另一個threadID
-        # 直接打印結果就好
+        # Wait-Debugger
+        # 明確的表明是utf-8，如果少了powershell會把當它當成是ISO-8859-1的編碼
         curl "https://api.openai.com/v1/threads/$threadID/messages?limit=$limit&order=$order" `
-            -H "Content-Type: application/json" `
+            -H 'Content-Type: application/json; charset=utf-8' `
             -H "Authorization: Bearer $env:OPENAI_API_KEY" `
             -H "OpenAI-Beta: assistants=v1"
+        <#
+        還是會遇到亂碼的問題
+        $obj # 先打印結果
+
+        $obj = $obj | ConvertFrom-Json # 將字串轉成json物件
+        if ($obj.data.Length -lt 2) {
+            Write-Host '此Thread下的訊息少於2(因此沒有assid)，建議可以呼叫DeleteThread來刪除'
+            Write-Host ('Request-OpenAI-DeleteThread {0}' -f $threadID) -ForegroundColor Red
+            continue
+        }
+
+        $asstID = $obj.data[1].assistant_id # 通常第二筆就會是機器人的回答，所以會有asstID
+        Request-OpenAI-OpenPlayground $threadID $asstID # 直接用playground打開
+        #>
         # $out += $obj # 出來的是亂碼
     }
     # return $out
